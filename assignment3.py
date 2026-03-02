ae544a9148f8248425333907793d644a2dc2a50b
Adam6120 <adam.hussain.2022@uni.strath.ac.uk>
2026-03-02 00:43:05 +0000

    Updated Progress with phi defined with boundary conditions

--- assignment3.py
+++ assignment3.py
@@ -34,7 +34,7 @@ N = 100
 L = 1          # 1 meter by 1 meter as per question 3                
 h = L / (N-1)  # Grid = N x N = 100 x 100 = 10,000 points
                # Assuming the grid points touch the boundary, if N = 100, there are 99 gaps, then the spacing is the length of one side over number of gap.
-omega = 2 / 1 + np.sin(np.pi / N)
+omega = 2 / (1 + np.sin(np.pi / N))
 
 # Defining charge functions f_ij
 
@@ -58,9 +58,26 @@ def charge_uniform_gradient(i,j):
 def charge_exponential_decay(i,j):
     return np.exp(-10 * np.sqrt((i*h - 0.5)**2 + (j*h -0.5)**2))
     
-    
-    
+#Defining phi with an empty array of size NxN, and applying boundary conditions
+#Boundary Conditions: (: to select all of the dimension)
+#                      (0 to select the first edge)
+#                      (N-1 to select the last edge)
 
+phi = np.zeroes([N,N]) # N rows and N columns (Rows is left to right, columns is up and down)
+#If N=100, the rows go from 0 to 99, so the top row is index 99 = N-1
+phiTop = phi[N-1, :] # [, :] is how to select all columns in numpy
+phiBottom = phi[0, :] #N-1 selects only one corner, so use colon to get entire edge
+phiLeft = phi[:, 0] #See notebook for deeper explanation
+phiRight = phi[:, N-1]
 
-for i in range(i,j):
-    phi_dash[i,j] = omega * (f[i,j] + 0.25*(phi_dash[i+1,j] + phi_dash[i-1,j] + phi_dash[i,j+1] + phi_dash[i,j-1])) + (1-omega) * phi[i,j]
+#Defining f, the charge function which is comprised of interior charges
+#f_ij * h^2 from the neighbours formula.
+#Double loop to get an i and j line, and then the whole grid.
+#Think about two perpindicular lines combining to make an entire square.
+f[i,j] = 
+
+#i and j are undefined, from N = 1 to 100, or in terms of python arrays: 1 - N-1
+#
+for i in range(1, N-1):
+    for j in range(1, N-1):
+        phi[i,j] = omega * (f[i,j] + 0.25*(phi[i+1,j] + phi[i-1,j] + phi[i,j+1] + phi[i,j-1])) + (1-omega) * phi[i,j]
